70c2d599064e4c7f6184b9c7c7792351d72f1780
aliciaamaliaalvarezmon <aliciaysuerte@gmail.com>
2018-04-07 20:26:38 -0300

    bueno no, ej 1 no terminado pero en progreso, falta threads y testeo

---
+++ TP1- pthreads/entregable/ConcurrentHashMap.hpp
@@ -0,0 +1,122 @@
+#ifndef HASHMAP_CONCURRENTE_H__
+#define HASHMAP_CONCURRENTE_H__
+#include <iostream>
+#include "ListaAtomica.hpp"
+
+//template <typename T>
+using namespace std;
+
+class ConcurrentHashMap{
+private:
+	Lista<pair<string, int>> _entradas[26]; //hay 26 letras en el ABC
+	//Lista<pair<string, int>>* _inicio;
+
+int hash_func(string key){
+	 int numero = key[0] - '0';
+	return (numero);
+}
+
+public:	
+	ConcurrentHashMap();
+	
+	~ConcurrentHashMap();
+
+	void addAndInc(string key){
+		int pos = hash_func(key);
+		Lista<pair<string, int>>::Iterador it =_entradas[pos].CrearIt();
+		bool esta = false;
+		while(it.HaySiguiente() and !esta){
+			if(it.Siguiente().first ==key){
+				esta = true;
+				it.Siguiente().second++;				
+			}else{
+				it.Avanzar();
+			}
+
+		} 
+		if (esta == false){
+			_entradas[pos].push_front( pair<string, int>(key, 1));
+		}
+
+	}
+
+	bool member(string key){
+		int pos = hash_func(key);
+		Lista<pair<string, int>>::Iterador it =_entradas[pos].CrearIt();
+		while(it.HaySiguiente()){
+			if(it.Siguiente().first == key){			
+				return true;			
+			}else{
+				it.Avanzar();
+			}
+		} 
+		return false;
+	}
+
+	pair<string, unsigned int>maximum(unsigned int nt){
+		//version sin concurrrencia nt = 1
+		pair<string, int> maximo("", 0);
+		for(i=0; i < 26; i++){			
+			Lista<pair<string, int>>::Iterador it =_entradas[pos].CrearIt();
+			while(it.HaySiguiente){
+				if(it.Siguiente().second > maximo.second){
+					maximo = it.Siguiente; 
+				}
+				it.Avanzar();
+			}			
+		}
+		return maximo;
+
+	}
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* HASHMAP_CONCURRENTE_H__ */
