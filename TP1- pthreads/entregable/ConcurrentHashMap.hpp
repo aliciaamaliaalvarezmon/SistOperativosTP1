c7d22b9d1649f0b3b62b85767c5006f5e7d27a29
aliciaamaliaalvarezmon <aliciaysuerte@gmail.com>
2018-04-07 21:24:34 -0300

    compila sin threads, nose los constructores, probando funciones

--- TP1- pthreads/entregable/ConcurrentHashMap.hpp
+++ TP1- pthreads/entregable/ConcurrentHashMap.hpp
@@ -12,17 +12,21 @@ private:
 	//Lista<pair<string, int>>* _inicio;
 
 int hash_func(string key){
-	 int numero = key[0] - '0';
+	 int numero = key[0] - '0'- 49;
 	return (numero);
 }
 
 public:	
-	ConcurrentHashMap();
-	
-	~ConcurrentHashMap();
+	/*ConcurrentHashMap(){
+		for(int i = 0; i < 26; i++){			
+			_entradas[i] = (new (Lista<pair<sting, int>>()));
+		}
+	}
+
+	~ConcurrentHashMap();*/
 
 	void addAndInc(string key){
-		int pos = hash_func(key);
+		int pos = hash_func(key);		
 		Lista<pair<string, int>>::Iterador it =_entradas[pos].CrearIt();
 		bool esta = false;
 		while(it.HaySiguiente() and !esta){
@@ -56,11 +60,11 @@ public:
 	pair<string, unsigned int>maximum(unsigned int nt){
 		//version sin concurrrencia nt = 1
 		pair<string, int> maximo("", 0);
-		for(i=0; i < 26; i++){			
-			Lista<pair<string, int>>::Iterador it =_entradas[pos].CrearIt();
-			while(it.HaySiguiente){
+		for(int i=0; i < 26; i++){			
+			Lista<pair<string, int>>::Iterador it =_entradas[i].CrearIt();
+			while(it.HaySiguiente()){
 				if(it.Siguiente().second > maximo.second){
-					maximo = it.Siguiente; 
+					maximo = it.Siguiente(); 
 				}
 				it.Avanzar();
 			}			
