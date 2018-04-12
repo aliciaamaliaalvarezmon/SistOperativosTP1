1e21770c40d2c4c2e77ab2eefc17dca179ae2480
aliciaamaliaalvarezmon <aliciaysuerte@gmail.com>
2018-04-11 23:12:36 -0300

    cambie la estructura, funciona maximum con 1 thread

--- TP1- pthreads/entregable/ConcurrentHashMap.cpp
+++ TP1- pthreads/entregable/ConcurrentHashMap.cpp
@@ -1,5 +1,6 @@
 #include "ConcurrentHashMap.h"
 #include <mutex>
+
 //template <typename T>
 
 
@@ -9,8 +10,9 @@ using namespace std;
 std::mutex m;
 std::mutex m2;
 
-ConcurrentHashMap::ConcurrentHashMap(){
-	_entradas = new( Lista<pair<string, int>>* [26]);
+/*ConcurrentHashMap::ConcurrentHashMap(){
+	_entradas = new (Lista<pair<string, int>>*[26]);
+	cout << (*_entradas).size() << endl;
 	for(int i = 0; i< 26; i++){
 		Lista<pair <string, int>>* lista = new (Lista<pair <string, int>>);
 		_entradas[i] = lista;		
@@ -24,11 +26,28 @@ ConcurrentHashMap::~ConcurrentHashMap(){
 		delete _entradas[i];			
 	}
 	delete _entradas;
+}*/
+ConcurrentHashMap::ConcurrentHashMap(){	
+	for(int i = 0; i< 26; i++){		
+		Lista<pair<string, int> >* l= new (Lista<pair<string, int> >);				 
+		_entradas.push_back(l);
+	}
+	_maximo= pair<string, int >("", 0);
+	_ultima  = 0;
+	//void* operator new[] (std::size_t size);
 }
 
-void ConcurrentHashMap::addAndInc(string key){
-	int pos = hash_func(key);		
-	Lista<pair<string, int>>::Iterador it = (*_entradas)[pos].CrearIt();
+ConcurrentHashMap::~ConcurrentHashMap(){
+	for(int i = 0; i < 26; i++){
+		delete(_entradas[i]);
+	}
+}
+
+
+
+void ConcurrentHashMap::addAndInc(string key){	
+	int pos = hash_func(key);	
+	Lista<pair<string, int>>::Iterador it = (*_entradas[pos]).CrearIt();
 	bool esta = false;
 	while(it.HaySiguiente() and !esta){
 		if(it.Siguiente().first ==key){			
@@ -41,15 +60,16 @@ void ConcurrentHashMap::addAndInc(string key){
 	} 
 	if (esta == false){
 		//parte critica en ej 6
-		(*_entradas)[pos].push_front( pair<string, int>(key, 1));
+		(*_entradas[pos]).push_front(pair<string, int>(key, 1));
 	}
+
 }
 
 bool ConcurrentHashMap::member(string key){
-	int pos = hash_func(key);
-	Lista<pair<string, int>>::Iterador it =(*_entradas)[pos].CrearIt();
+	int pos = hash_func(key);	
+	Lista<pair<string, int>>::Iterador it =(*_entradas[pos]).CrearIt();
 	while(it.HaySiguiente()){
-		if(it.Siguiente().first == key){			
+		if(it.Siguiente().first == key){						
 			return true;			
 		}else{
 			it.Avanzar();
@@ -60,23 +80,22 @@ bool ConcurrentHashMap::member(string key){
 
 
 
-void * ConcurrentHashMap::maxaux(){	
-		while(_ultima < 26){	
+void * ConcurrentHashMap::maxaux(){		
+		while(_ultima < 26){										
 			m.lock();
 			Lista<pair<string, int>>::Iterador it =(*_entradas[_ultima]).CrearIt();
-			_ultima++;
-			m.unlock();
-			while(it.HaySiguiente()){
-				if(it.Siguiente().second >= _maximo.second){
+			_ultima++;				
+			m.unlock();			
+ 			while(it.HaySiguiente()){				
+				if(it.Siguiente().second >= _maximo.second){					
 					m2.lock();
-					_maximo = it.Siguiente(); 
-					m2.unlock();
+					_maximo = it.Siguiente();
+					m2.unlock();					
 				}
 				it.Avanzar();
-			}			
-		}		
-		
-			return nullptr;
+			}				
+		}			
+		return nullptr;
 		
 	}
 
@@ -88,7 +107,7 @@ void * ConcurrentHashMap::maxaux(){
 
 	}
 
-	pair<string, int> ConcurrentHashMap::maximum(unsigned int nt){
+	pair<string, int> ConcurrentHashMap::maximum(unsigned int nt){		
 		int realnt;
 		if(nt <= 26){
 			realnt = nt;
@@ -107,33 +126,28 @@ void * ConcurrentHashMap::maxaux(){
 		for (tid = 0; tid < realnt; ++tid){
          pthread_join(thread[tid], NULL);
      	}
+     	_ultima = 0;
      	return _maximo;
 	}
 
 
-
 /*
+
 pair<string, int> ConcurrentHashMap::maximum(unsigned int nt){
-	int realnt;
-	if(nt <= 26){
-		realnt = nt;
-	}else{
-		realnt = 26;
-	}		
-	pthread_t thread[realnt];
-	pair<int, int> tids[realnt];
-	int tid;
-	
-	for(tid = 0; tid <  nt; tid++  ){
-		tids[tid].first = tid;
-		tids[tid].second = realnt;
-		pthread_create(&thread[tid], NULL, maxaux, &tids[tid]);//le pasa a maxaux la thread actual y la cantidad de threads.
+		//version sin concurrrencia nt = 1
+
+		pair<string, int> maximo("", 0);
+		for(int i=0; i < 26; i++){			
+			Lista<pair<string, int>>::Iterador it =(*_entradas[i]).CrearIt();
+			while(it.HaySiguiente()){
+				if(it.Siguiente().second > maximo.second){
+					maximo = it.Siguiente(); 
+				}
+				it.Avanzar();
+			}			
+		}
+		return maximo;
+ 
 	}
 
-	for (tid = 0; tid < realnt; ++tid){
-        pthread_join(thread[tid], NULL);
-   	}
-   	return _maximo;
-}
 */
-
--- TP1- pthreads/entregable/ConcurrentHashMap.h
+++ TP1- pthreads/entregable/ConcurrentHashMap.h
@@ -4,17 +4,22 @@
 #include <pthread.h>
 #include <mutex>
 #include "ListaAtomica.hpp"
+#include <vector>
 //template <typename T>
 using namespace std;
 
 
 class ConcurrentHashMap{
 private:
-	Lista<pair<string, int>>** _entradas; //hay 26 letras en el ABC
+	/*Lista<pair<string, int>>** _entradas; //hay 26 letras en el ABC
 	pair<string, int> _maximo; // pair<string, int> maximo("", 0), sino es asi por default va a ver que cambiarlo;
 	int _ultima;
+*/
+	vector<Lista<pair<string, int> >* > _entradas; //hay 26 letras en el ABC
+	pair<string, int> _maximo; // pair<string, int> maximo("", 0), sino es asi por default va a ver que cambiarlo;	
+    int  _ultima; 	
 	int hash_func(string key){
-		 int numero = key[0] - '0'- 49;
+	 int numero = key[0] - '0'- 49;
 		return (numero);
 	}
 
@@ -27,24 +32,5 @@ public:
 	pair<string, int> maximum(unsigned int nt);
 };
 
-/*void ConcurrentHashMap::addAndInc(string key){
-		int pos = hash_func(key);		
-		Lista<pair<string, int>>::Iterador it =_entradas[pos].CrearIt();
-		bool esta = false;
-		while(it.HaySiguiente() and !esta){
-			if(it.Siguiente().first ==key){
-				esta = true;
-				it.Siguiente().second++;				
-			}else{
-				it.Avanzar();
-			}
 
-		} 
-		if (esta == false){
-			_entradas[pos].push_front( pair<string, int> (key, 1));
-		}
-
-	}
-
-*/
 #endif /* HASHMAP_CONCURRENTE_H__ */
